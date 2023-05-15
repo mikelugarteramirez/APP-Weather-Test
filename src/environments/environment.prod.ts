6c4bb49c72b301e8c6cf0d3de2193747c4665820
Mikel Ugarte <mikel.ugarte@ab-inbev.com>
2023-05-15 13:53:19 -0500

    Add production enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://api.openweathermap.org',
+  apiKey: '4371ee1d6c62160ac6e116e30b17af85',
+  mapsApiKey: 'AIzaSyAsBt7D0n61fx-Yt02UthYBPtDZeMnALLY'
 };
